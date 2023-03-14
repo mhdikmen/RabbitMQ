7bc24b42e171c75962813e95f38e3deb21723a6a
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2023-03-14 10:13:59 +0300

    Add project files.

---
+++ Publisher/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+using RabbitMQ.Client;
+using System.Runtime.InteropServices;
+using System.Text;
+
+Console.WriteLine("Hello, World!");
+
+var factory = new ConnectionFactory();
+factory.Uri = new Uri("amqps://zrdbzcyq:VAy_ksplaQG0kI-pza5_AzM_k-so7Tcz@possum.lmq.cloudamqp.com/zrdbzcyq");
+
+var connection = factory.CreateConnection();
+
+var channel = connection.CreateModel();
+
+
+channel.ExchangeDeclare("fanout-type", ExchangeType.Fanout, true, false, null);
+
+//channel.QueueDeclare("myqueue", true, false, false, null);
+
+Enumerable.Range(1, 50).ToList().ForEach(x =>
+{
+
+    string message = $"Message {x}";
+
+    var messageBody = Encoding.UTF8.GetBytes(message);
+
+    channel.BasicPublish("fanout-type", string.Empty, null, messageBody);
+
+    Console.WriteLine($"Mesaj gönderilmiştir. = {message}");
+
+});
+
---
+++ Subscriber/Program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+Console.WriteLine("mesajlar dinleniyor....");
+var factory = new ConnectionFactory();
+factory.Uri = new Uri("amqps://zrdbzcyq:VAy_ksplaQG0kI-pza5_AzM_k-so7Tcz@possum.lmq.cloudamqp.com/zrdbzcyq");
+
+var connection = factory.CreateConnection();
+
+var channel = connection.CreateModel();
+
+channel.BasicQos(0, 10, true);
+
+
+var randomQueue = channel.QueueDeclare(exclusive: false, durable: false, autoDelete: true);
+
+
+channel.QueueBind(randomQueue.QueueName, "fanout-type", string.Empty, null);
+
+var consumer = new EventingBasicConsumer(channel);
+
+consumer.Received += (sender, args) =>
+{
+
+	try
+	{
+        var message = Encoding.UTF8.GetString(args.Body.ToArray());
+        Console.WriteLine($"Gelen mesaj:{message}");
+        Thread.Sleep(500);
+		channel.BasicAck(args.DeliveryTag, false);
+    }
+	catch (Exception)
+	{
+
+		throw;
+	}
+  
+
+};
+
+channel.BasicConsume(randomQueue.QueueName, false, consumer);
+
+Console.ReadLine();
